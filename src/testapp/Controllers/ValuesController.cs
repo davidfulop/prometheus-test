f6b98e7d207e23f78c55a8ca9537e505de12aa0b
David Fulop <28536613+davidfulop@users.noreply.github.com>
2019-05-31 16:29:09 +0100

    Add test .NET Core app

---
+++ src/testapp/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testapp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return DateTime.UtcNow.ToString();
+        }
+    }
+}
